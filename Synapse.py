5fd5765340d77f15a1a246e2ff1e09899c8b182c
AlexeyNesterovichEst <64419626+AlexeyNesterovichEst@users.noreply.github.com>
2023-12-12 10:57:49 +0200

    Update Synapse.py

--- Synapse.py
+++ Synapse.py
@@ -6,8 +6,9 @@ db_token = st.secrets["DB_TOKEN"]
 syn = synapseclient.login(authToken=db_token)
 profile = syn.getUserProfile()
 
-project_name = 'MySynapseStreamlit'
+project_name = 'MySynapseStreamlitOne'
 project = Project(project_name)
 project = syn.store(project)
+st.success(project)
 entity = syn.get(project_name)
 st.success(entity)
